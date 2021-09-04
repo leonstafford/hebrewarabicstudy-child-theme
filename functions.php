ff1da933f562f55fbbdf596c2fc2a755eb7938dc
Leon Stafford <me@ljs.dev>
2021-09-04 15:33:31 +1000

    override site title

---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The functions.php file
+ *
+ * @package HebrewArabicChildTheme
+ */
+?>
+
+add_filter( 'wp_title', 'accessible_minimalism_wp_title_for_home' );
+ 
+/**
+ * Customize the title for the home page, if one is not set.
+ *
+ * @param string $title The original title.
+ * @return string The title to use.
+ */
+function accessible_minimalism_wp_title_for_home( $title )
+{
+  if ( empty( $title ) && ( is_home() || is_front_page() ) ) {
+    $title = __( 'Hebrew &amp; Arabic Study', 'hebrewarabicchild' ) . ' | ' . get_bloginfo( 'description' );
+  }
+  return $title;
+}
