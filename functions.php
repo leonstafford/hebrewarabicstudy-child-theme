e203cb957a2658455f6c921d843c167823c459f1
Leon Stafford <me@ljs.dev>
2021-09-17 22:31:17 +1000

    wip Resources template

--- functions.php
+++ functions.php
@@ -18,5 +18,17 @@ function accessible_minimalism_wp_title_for_home( $title )
   if ( empty( $title ) && ( is_home() || is_front_page() ) ) {
     $title = 'Hebrew &amp; Arabic Study';
   }
+
   return $title;
 }
+
+function accessible_minimalism_category_template( $template ) {
+    if ( cat_is_ancestor_of( 4, get_queried_object_id() /* The current category ID */ ) )
+    {
+        $template = locate_template( 'category-resource.php' );
+    }
+
+    return $template;
+}
+
+add_filter( 'category_template', 'accessible_minimalism_category_template' );
